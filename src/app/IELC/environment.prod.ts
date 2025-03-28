bd59b9c44a4d5cdf1f19b12a29aa61268a5e39b1
Akhilpasha Mohmad <Akhilpasha.m@inteqsolutions.com>
2025-03-28 15:58:07 +0530

    updated environment file

--- src/app/IELC/environment.prod.ts
+++ src/app/IELC/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     redirectUri: "https://sampleielc.azurewebsites.net",
-    postLogoutRedirectUri: "https://sampleielc.azurewebsites.net/login"
+    postLogoutRedirectUri: "https://sampleielc.azurewebsites.net/#/login"
 
   };
   
