7777dbb1386c6e1164d51809a117cb353e78a6c7
Akhilpasha Mohmad <Akhilpasha.m@inteqsolutions.com>
2025-05-22 11:08:14 +0530

    updated prod uri

--- src/app/IELC/environment.prod.ts
+++ src/app/IELC/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
-    redirectUri: "https://sampleielc.azurewebsites.net/authcallback",
-    postLogoutRedirectUri: "https://sampleielc.azurewebsites.net/#/login"
-
+    // redirectUri: "https://sampleielc.azurewebsites.net/authcallback",
+    // postLogoutRedirectUri: "https://sampleielc.azurewebsites.net/#/login"
+     redirectUri: "https://inteqemployeelearingcenter.azurewebsites.net/authcallback",
+    postLogoutRedirectUri: "https://inteqemployeelearingcenter.azurewebsites.net/#/login"
   };
   
