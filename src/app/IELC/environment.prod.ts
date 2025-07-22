4af09438d71b70eaacf046fc81c3ff9e0f43aaf4
Navya Bavana <navya.bavana@inteqsolutions.com>
2025-07-22 15:29:50 +0530

    responsive changes for addnewskill,registeredusers

--- src/app/IELC/environment.prod.ts
+++ src/app/IELC/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     // redirectUri: "https://sampleielc.azurewebsites.net/authcallback",
     // postLogoutRedirectUri: "https://sampleielc.azurewebsites.net/#/login"
-     redirectUri: "https://inteqemployeelearingcenter.azurewebsites.net/authcallback",
-    postLogoutRedirectUri: "https://inteqemployeelearingcenter.azurewebsites.net/#/login"
+     redirectUri: "https://ielc-test.azurewebsites.net/authcallback",
+    postLogoutRedirectUri: "https://ielc-test.azurewebsites.net/#/login"
   };
   
