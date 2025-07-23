2dce3993b457823ac8f7da6abae3c4e63e5e5096
Navya Bavana <navya.bavana@inteqsolutions.com>
2025-07-23 19:19:47 +0530

    Changes regarding_ Login As Label

--- src/app/IELC/environment.prod.ts
+++ src/app/IELC/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     // redirectUri: "https://sampleielc.azurewebsites.net/authcallback",
     // postLogoutRedirectUri: "https://sampleielc.azurewebsites.net/#/login"
-    redirectUri: "https://ielc-test.azurewebsites.net/authcallback",
+  redirectUri: "https://ielc-test.azurewebsites.net/authcallback",
  postLogoutRedirectUri: "https://ielc-test.azurewebsites.net/#/login"
   };
   
