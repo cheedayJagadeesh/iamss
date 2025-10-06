900c4f6f0213fbedaefb04a73f1418320678d59f
Cheeday Durga Sai Krishna Jagadeesh <Jagadeesh.c@inteqsolutions.com>
2025-10-06 14:41:45 +0000

    Updated ielcapi.service.ts

--- src/app/IELC/ielcapi.service.ts
+++ src/app/IELC/ielcapi.service.ts
@@ -1579,7 +1579,7 @@ getEnrollmentSessionsBySkillAndDateRange(skillName: string, dateRange: string): 
 
     Updatecisoismsguideline(id: number, updatedData: any): Observable<any> {
       const headers = this.getHeaders();
-      return this.http.put<any>(`${this.cisoismsguidelineUrl}/${id}`, updatedData,{ observe: 'response' });
+      return this.http.put<any>(`${this.cisoismsguidelineUrl}/${id}`, updatedData,{headers, observe: 'response' });
     }
 
     DeletecisoismsguidelineById(id: number): Observable<void> {
@@ -1601,7 +1601,7 @@ getEnrollmentSessionsBySkillAndDateRange(skillName: string, dateRange: string): 
 
     Updatecisoismspolicy(id: number, updatedData: any): Observable<any> {
       const headers = this.getHeaders();
-      return this.http.put<any>(`${this.cisoismspolicyUrl}/${id}`, updatedData,{ observe: 'response' });
+      return this.http.put<any>(`${this.cisoismspolicyUrl}/${id}`, updatedData,{headers, observe: 'response' });
     }
 
     DeletecisoismspolicyById(id: number): Observable<void> {
@@ -1623,7 +1623,7 @@ getEnrollmentSessionsBySkillAndDateRange(skillName: string, dateRange: string): 
 
     Updatecisoismsprocedure(id: number, updatedData: any): Observable<any> {
       const headers = this.getHeaders();
-      return this.http.put<any>(`${this.cisoismsprocedureUrl}/${id}`, updatedData,{ observe: 'response' });
+      return this.http.put<any>(`${this.cisoismsprocedureUrl}/${id}`, updatedData,{headers, observe: 'response' });
     }
 
     DeletecisoismsprocedureById(id: number): Observable<void> {
@@ -1645,7 +1645,7 @@ getEnrollmentSessionsBySkillAndDateRange(skillName: string, dateRange: string): 
 
     Updatecisoismsformat(id: number, updatedData: any): Observable<any> {
       const headers = this.getHeaders();
-      return this.http.put<any>(`${this.cisoismsformatUrl}/${id}`, updatedData,{ observe: 'response' });
+      return this.http.put<any>(`${this.cisoismsformatUrl}/${id}`, updatedData,{headers, observe: 'response' });
     }
 
     DeletecisoismsformatById(id: number): Observable<void> {
