8a2a3bcd64065372fa8c0e3323e310cb4dca69c9
Cheeday Durga Sai Krishna Jagadeesh <jagadeesh.c@inteqsolutions.com>
2025-12-23 11:09:41 +0530

    custom domain added for test

--- src/app/IELC/environment.prod.ts
+++ src/app/IELC/environment.prod.ts
@@ -1,8 +1,8 @@
-export const environment = {
-    production: true,
-    redirectUri: "https://ielc-test.azurewebsites.net/authcallback",
-    postLogoutRedirectUri: "https://ielc-test.azurewebsites.net/#/login"
-  };
+// export const environment = {
+//     production: true,
+//     redirectUri: "https://ielc-test.azurewebsites.net/authcallback",
+//     postLogoutRedirectUri: "https://ielc-test.azurewebsites.net/#/login"
+//   };
   
 // const isTest = window?.location?.hostname.includes("ielc.inteqportal.com");
 
@@ -26,3 +26,38 @@ export const environment = {
 //     ? "https://ielc.inteqportal.com/#/login"
 //     : "https://inteqemployeelearingcenter.azurewebsites.net/#/login"
 // };
+
+
+const hostname = window?.location?.hostname;
+
+const ENV_MAP: any = {
+  "ielc.inteqportal.com": {
+    redirectUri: "https://ielc.inteqportal.com/authcallback",
+    postLogoutRedirectUri: "https://ielc.inteqportal.com/#/login"
+  },
+  "app-local.xyz": {
+    redirectUri: "https://app-local.xyz/authcallback",
+    postLogoutRedirectUri: "https://app-local.xyz/#/login"
+  },
+   "www.app-local.xyz": {
+    redirectUri: "https://www.app-local.xyz/authcallback",
+    postLogoutRedirectUri: "https://www.app-local.xyz/#/login"
+  },
+  "inteqemployeelearingcenter.azurewebsites.net": {
+    redirectUri: "https://inteqemployeelearingcenter.azurewebsites.net/authcallback",
+    postLogoutRedirectUri: "https://inteqemployeelearingcenter.azurewebsites.net/#/login"
+  }
+};
+
+// fallback (local or unknown)
+const currentEnv =
+  ENV_MAP[hostname] ?? {
+    redirectUri: "http://localhost:4200/authcallback",
+    postLogoutRedirectUri: "http://localhost:4200/#/login"
+  };
+
+export const environment = {
+  production: true,
+  redirectUri: currentEnv.redirectUri,
+  postLogoutRedirectUri: currentEnv.postLogoutRedirectUri
+};
