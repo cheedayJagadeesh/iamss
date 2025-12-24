e6cb527425216dc54f8993f9c29b0fa800b3f8ad
Cheeday Durga Sai Krishna Jagadeesh <jagadeesh.c@inteqsolutions.com>
2025-12-24 18:42:36 +0530

    added test-dns in environment.ts

--- src/app/IELC/environment.ts
+++ src/app/IELC/environment.ts
@@ -11,11 +11,19 @@ export const environment = {
       redirectUri: 'http://localhost:4200/authcallback',
       postLogoutRedirectUri: 'http://localhost:4200/#/login'
     },
+    'test-dns': {
+      redirectUri: 'http://localhost:4200/authcallback',
+      postLogoutRedirectUri: 'http://localhost:4200/#/login'
+    },
     stage: {
       redirectUri: 'http://localhost:4200/authcallback',
       postLogoutRedirectUri: 'http://localhost:4200/#/login'
     },
     prod: {
+      redirectUri: 'http://localhost:4200/authcallback',
+      postLogoutRedirectUri: 'http://localhost:4200/#/login'
+    },
+    "prod-dns": {
       redirectUri: 'http://localhost:4200/authcallback',
       postLogoutRedirectUri: 'http://localhost:4200/#/login'
     }
